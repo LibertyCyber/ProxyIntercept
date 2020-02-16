053a5390ccc6bb653f5eaf01297cc4a9af196bac
Pogo_2 <germanyben64@gmail.com>
2020-02-15 23:52:11 -0800

    saving for tonight

---
+++ get_requests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import socket
+import time
+import requests
+from proxy_interceptor import Intercept
+
+port = 8080
+
+
+while True:
+    s = socket.socket()          
+    s.bind(('', port))
+    s.listen(5)
+
+    (c, addr) = s.accept()
+    rec = (c.recv(1024))
+
+    rdata = rec.decode('utf-8')
+    currentobj = Intercept(rdata)
+    
+    print(currentobj)
+    
+    correct_requ = input("save this to your collection(y/n): ")
+
+    if(correct_requ == "y" or correct_requ == "Y"):
+        break
+    elif(correct_requ == "n" or correct_requ == "N"):
+        print("continuing scan")
+        c.send(b"HTTP/1.1 200 OK")
+    
---
+++ proxy_interceptor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import socket
+import requests
+
+class Intercept:
+
+    def __init__(self,source):
+        self.source = source
+        
+    def __repr__(self):
+        return self.source
+        
+    
+
+    
+if __name__ == "__main__":
+    print("done")
